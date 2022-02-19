eaae5b4eaa4f82f273644e00402260d9acd62be0
Joël Karhambae <joelkarhamba@hotmail.com>
2022-02-19 16:57:43 +0100

    mainService.js

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@
 const Header = () => {
     return (
     <header id='wrapper' className="container-fluid">
+        
         <div className="header-Home row">
             <h1> Experten in verhuis</h1>
             <p>BILLIET QUALITY MOVERS verhuist uw meubels en goederen met de GROOTST MOGELIJKE ZORG. Met onze uitstekende transportservice over het volledige nationale grondgebied garanderen wij een veilige verhuis van al uw waardevolle bezittingen.</p>
--- components/Main-services.js
+++ components/Main-services.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const MainServices = () => {
@@ -5,31 +6,40 @@ const MainServices = () => {
         <section className="container-fluid service-container">
             <h2>Diensten</h2>
             <section className="row">
-                <article className="col-lg">
-                    
-                    <h3>Inpakservice</h3>
-                    <p>Een succesvolle en vlotte verhuis start met een goed georganiseerde
-                        voorbereiding en inpakwerk. De juiste dozen en beschermingsmateriaal zijn van
-                        vitaal belang voor de veiligheid van uw bezittingen.</p>
+                {data.map(({id, title, src, description})=>(
+                    <article key={id} className="col-xl">
+                    <Image alt="l" src={src} width={80} height={80} objectFit='cover'/>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                    <button><a href="#">More...</a></button>
                 </article>
-                <article className="col-lg">
-                    <h3>Inpakmateriaal</h3>
-                    <p>VERHUISDOZEN EN INPAKMATERIALL: BILLIET QUALITY MOVERS® heeft een grote
-                        selectie aan verhuisdozen en inpakmateriaal aan voordelige prijzen: plakband,
-                        inpakpapier, plasticfolie en bubbelfolie (gelieve de bubbels niet te poppen tot
-                        de verhuis werd afgerond).
-                    </p>
-                </article>
-                <article className="col-lg">
-                    <h3>Cross docking</h3>
-                    <p>Grote meubelstukken en toestellen kunnen niet altijd via de trap of de lift
-                        naar boven of naar beneden. Het alternatief is de meubellift. Indien U verhuist
-                        met BILLIET QUALITY MOVERS® , dan wordt deze service (optioneel) aan een
-                        voordelige prijs aangeboden.</p>
-                </article>
+                ))}
             </section>
         </section>
     )
 }
 
 export default MainServices
+
+export const data = [
+    {
+        id:1,
+        title: "Inpakservice",
+        src:"/handing-package.svg",
+        description: "Een succesvolle en vlotte verhuis start met een goed georganiseerde voorbereiding en inpakwerk. De juiste dozen en beschermingsmateriaal zijn van vitaal belang voor de veiligheid van uw bezittingen."
+    },
+    {
+        id:2,
+        title: "Inpakmateriaal",
+        src:"/box.svg",
+        description: `VERHUISDOZEN EN INPAKMATERIALL: BILLIET QUALITY MOVERS® heeft een grote selectie aan verhuisdozen en inpakmateriaal aan voordelige prijzen: plakband, inpakpapier, plasticfolie en bubbelfolie 
+        (gelieve de bubbels niet te poppen tot de verhuis werd afgerond).`
+    },
+    {
+        id:3,
+        title: "Cross Docking",
+        src:"/kraan.svg",
+        description: `Grote meubelstukken en toestellen kunnen niet altijd via de trap of de lift naar boven of naar beneden. Het alternatief is de meubellift. Indien U verhuist met BILLIET QUALITY MOVERS® , 
+        dan wordt deze service (optioneel) aan een voordelige prijs aangeboden.`
+    }
+]
